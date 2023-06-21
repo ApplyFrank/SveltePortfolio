cb04886a2936a666b3be8ae89caf992bf0ce795d
Frank Su <frank.su@applydigital.co>
2023-06-21 15:06:21 -0700

    Setup each pages header

---
+++ svelte-portfolio/src/lib/Navbar.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { browser } from "$app/environment";
+
+    export let position = 0
+
+    let menu = false;
+
+    const links: [string, string][] = [
+        ["/#about", "About"], //why is there a # infront?
+        ["/#experience", "Experience"],
+        ["/#projects", "Projects"],
+        ["/#contact", "Contact"]
+    ]
+
+    let scroll: boolean
+    // $: if (browser) scroll = document.body.classList.contains("scroll") || position != 0;
+
+</script>
+<h1>Navbar</h1>
--- svelte-portfolio/src/routes/+page.svelte
+++ svelte-portfolio/src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import type { Writable } from "svelte/store"
+
+    import About from "../lib/index/About.svelte"
+    import Experience from "../lib/index/Experience.svelte"
+    import Projects from "../lib/index/Projects.svelte"
+    import Contact from "../lib/index/Contact.svelte"
+    import Navbar from "../lib/Navbar.svelte"
+    import { browser } from "$app/environment"
+	import { bind } from "svelte/internal"
+
+    let mount = browser
+
+    const sections = ["about", "experience", "projects", "contact"];
+
+    let position: Writable<number>;
+    let active: { [key: number]: boolean } = {}
+
+</script>
+
+<svelte:head>
+    <title>FrankIsCool</title>
+</svelte:head>
+
+{#each sections as section}<div id={section} />{/each}
+
+
+<div class="wrapper" class:mount>
+    <!-- <Navbar bind:position={$position} /> -->
+    <main class="h-screen">
+        <About />
+        <Experience />
+        <Projects />
+        <Contact />
+    </main>
+</div>
